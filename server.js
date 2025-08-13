840fd60a59e78c09d5a2864e184b70a96799da6e
GaelKayombo <Gael.k.Kayombo@gmail.com>
2025-08-13 07:16:07 +0100

    starting point server file

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-// test run commit see if this even works lol
-console.log("just checking if git is picking this up");
+// starting point for the app
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from my DCWA project');
+});
+
+app.listen(3000, () => {
+  console.log('server running on port 3000');
+});
